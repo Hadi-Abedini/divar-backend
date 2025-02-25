4a7bf12114ff6f09fa6c99a58e50498c8b7f5836
Hadi-Abedini <hadi.z.abedini@gmail.com>
2025-02-25 18:12:54 +0330

    feat: configure TypeORM with PostgreSQL and add dotenv support

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
